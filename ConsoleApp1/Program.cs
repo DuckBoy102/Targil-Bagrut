84e78ad3288e20ff80d4adf9adf6d55f5b59fe38
DuckBoy102 <avieln7@gmail.com>
2022-02-24 16:47:14 +0200

    hay

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,6 +10,26 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            double b = 0;
+            int a = 0;
+            int battaris = 0;
+            int num_of_purches = 0;
+            double money = 0;
+            Console.WriteLine("enter the num of battaris");
+            battaris = int.Parse(Console.ReadLine());
+            while (battaris >= 10)
+            {
+                Console.WriteLine("how many battarais do u need");
+                a = int.Parse(Console.ReadLine());
+                b = a * 2.5;
+                Console.WriteLine(b);
+                battaris = battaris - a;
+                num_of_purches = num_of_purches + 1;
+                money = money + b;
+            }
+            Console.WriteLine("the num of purchess is {0}",num_of_purches);
+            Console.WriteLine("the num of money that we got is {0}",money);
+
         }
     }
 }
